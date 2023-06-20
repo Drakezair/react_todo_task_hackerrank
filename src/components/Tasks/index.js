748619eb5fcb39db62ed0acb9efff4b8fadbc895
luis andrade <luis.andrade@treinta.co>
2023-06-20 02:12:43 -0400

    wip: useTasks

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,37 +1,69 @@
 import React from "react";
 import "./index.css";
+import useTasks from "../../hooks/useTask";
 
 function Tasks() {
+  const {
+    tasks,
+    task,
+    priority,
+    handleTaskChange,
+    handlePriorityChange,
+    handleAddTask,
+    handleDeleteTask,
+  } = useTasks();
 
-    return (
-        <div className="layout-column align-items-center justify-content-start">
-            <section className="layout-row align-items-center justify-content-center mt-30">
-                <input data-testid="input-task-name" className="large mx-8"
-                       placeholder="Task Title"/>
-                <input data-testid="input-task-priority" className="large mx-8"
-                       placeholder="Task Priority"/>
-                <button data-testid="submit-button">Add Task</button>
-            </section>
-
-            <div className="card w-40 pt-30 pb-8 mt-20">
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Priority</th>
-                    </tr>
-                    </thead>
-                    <tbody data-testid="tasksList">
-                            <tr>
-                                <td>Task</td>
-                                <td>1</td>
-                                <td><button className="danger">Delete</button></td>
-                            </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="layout-column align-items-center justify-content-start">
+      <section className="layout-row align-items-center justify-content-center mt-30">
+        <input
+          data-testid="input-task-name"
+          className="large mx-8"
+          placeholder="Task Title"
+          type="text"
+          value={task}
+          onChange={handleTaskChange}
+        />
+        <input
+          data-testid="input-task-priority"
+          className="large mx-8"
+          value={priority}
+          placeholder="Task Priority"
+          type="number"
+          onChange={handlePriorityChange}
+        />
+        <button onClick={handleAddTask} data-testid="submit-button">
+          Add Task
+        </button>
+      </section>
+      <div className="card w-40 pt-30 pb-8 mt-20">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Priority</th>
+            </tr>
+          </thead>
+          <tbody data-testid="tasksList">
+            {tasks.map(({ task, priority }, index) => (
+              <tr>
+                <td>{task}</td>
+                <td>{priority}</td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteTask(index)}
+                    className="danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Tasks
+export default Tasks;
---
+++ src/hooks/useTask.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export default function useTasks() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+  const [priority, setPriority] = useState("");
+
+  const handleTaskChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handlePriorityChange = (event) => {
+    setPriority(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (!task || !priority)
+      window.alert("Please enter both title and task priority");
+    setTasks(
+      [...tasks, { task, priority }].sort((a, b) => b.priority - a.priority)
+    );
+    setTask("");
+    setPriority("");
+  };
+
+  const handleDeleteTask = (index) => {
+    if (index === 1) {
+      setTasks([]);
+      return;
+    }
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  return {
+    tasks,
+    task,
+    priority,
+    handleTaskChange,
+    handlePriorityChange,
+    handleAddTask,
+    handleDeleteTask,
+  };
+}
